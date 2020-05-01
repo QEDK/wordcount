8715af899e4a44988a400be391ab2c82508ec319
ankit <1994constant@gmail.com>
2020-05-01 19:15:36 +0530

    New features, security headers, et al.

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request
+from flask_talisman import Talisman
 import re
 
 app = Flask(__name__)
+csp = {
+	"default-src": "'self'",
+	"style-src": ["'self'", "https://fonts.googleapis.com"],
+	"font-src": "https://fonts.gstatic.com"
+}
+talisman = Talisman(app, content_security_policy=csp)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -9,11 +16,17 @@ def home():
 	text = None
 	wordcount = 0
 	linecount = 0
+	charcount = 0
 	if request.method == "POST":
 		text = request.form["wordcount"]
-		wordcount = len(re.split(r"\w\b", text)) - 1
-		linecount = len(text.split("\n"))
-	return render_template("index.html", text=text, wordcount=wordcount, linecount=linecount)
+		charcount = len(text)
+		if charcount == 0:
+			pass
+		else:
+			charcount = len(re.split(r"[^\n\r]", text)) - 1
+			wordcount = len(re.split(r"\w\b", text)) - 1
+			linecount = text.count("\n") + 1
+	return render_template("index.html", text=text, wordcount=wordcount, charcount=charcount, linecount=linecount)
 
 
 if __name__ == "__main__":
